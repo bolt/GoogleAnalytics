d893c63d81b07bae6364893fbc5f001a43a4cc85
Aaron Valandra <avaland@woopta.com>
2016-07-28 20:31:29 -0500

    Massive refactor of Google Analytics plugin for V3.

--- src/GoogleAnalyticsExtension.php
+++ src/GoogleAnalyticsExtension.php
@@ -6,50 +6,51 @@ namespace Bolt\Extension\Bolt\GoogleAnalytics;
 use Bolt\Asset\Snippet\Snippet;
 use Bolt\Asset\Target;
 use Bolt\Controller\Zone;
+use Bolt\Extension\Bolt\GoogleAnalytics\Controller\StatisticsController;
+use Bolt\Extension\Bolt\GoogleAnalytics\Provider\GoogleAnalyticsProvider;
 use Bolt\Extension\SimpleExtension;
 use Bolt\Menu\MenuEntry;
-use Eloquent\Pathogen\Exception\EmptyPathAtomException;
-use Silex\ControllerCollection;
 use Bolt\Translation\Translator as Trans;
-use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Translation\Loader as TranslationLoader;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
+/**
+ * Class GoogleAnalyticsExtension
+ * @package Bolt\Extension\Bolt\GoogleAnalytics
+ */
 class GoogleAnalyticsExtension extends SimpleExtension
 {
 
-    protected function registerServices(Application $app)
+    /**
+     * @return array
+     */
+    public function getServiceProviders()
     {
-        //Register translations in the service provider.
-        $translationDirectory = $this->getBaseDirectory()->getDir('translations');
-        if ($translationDirectory->exists()) {
-            foreach ($translationDirectory->getContents(true) as $fileInfo) {
-                if ($fileInfo->isFile()) {
-                    list($domain, $extension) = explode('.', $fileInfo->getFilename());
-
-                    $path = $app['resources']->getPath('extensions/' . $fileInfo->getPath());
-
-                    $app['translator']->addResource($extension, $path, $domain);
-                }
-            }
-        }
-
-        parent::registerServices($app);
+        return [
+            $this,
+            new GoogleAnalyticsProvider($this->getConfig(), $this->getBaseDirectory())
+        ];
     }
 
-    protected function registerBackendRoutes(ControllerCollection $collection)
+    /**
+     * @return array
+     */
+    protected function registerBackendControllers()
     {
-        $collection->match('/extensions/google-analytics', [$this, 'googleAnalytics']);
+        return [
+            '/extensions' => new StatisticsController($this->getContainer()),
+        ];
     }
 
+    /**
+     * @return array
+     * If backend is set to false, don't load the menu link.
+     */
     protected function registerMenuEntries()
     {
-        $config = $this->getConfig();
+        $app = $this->getContainer();
 
-        if ($config['backend'] === false) {
-            return;
+        if (! $app['ga.config.config']->isBackend()) {
+            return [];
         }
 
         $menu = (new MenuEntry('google', '/bolt/extensions/google-analytics'))
@@ -61,142 +62,28 @@ class GoogleAnalyticsExtension extends SimpleExtension
         ];
     }
 
+    /**
+     * @return array
+     */
     protected function registerAssets()
     {
+        $app = $this->getContainer();
+
         $analyticsCode = (new Snippet())
             ->setZone(Zone::FRONTEND)
             ->setLocation(Target::END_OF_HEAD)
-            ->setCallback([$this, 'insertAnalytics']);
+            ->setCallback([$app['ga.snippet.analytics'], "insertAnalytics"]);
 
         return [
             $analyticsCode
         ];
     }
 
-    public function googleAnalytics(Application $app, Request $request)
+    /**
+     * @return array
+     */
+    protected function registerTwigPaths()
     {
-        //Block unauthorized access...
-        if (!$app['users']->isAllowed('dashboard')) {
-            throw new AccessDeniedException('Logged in user does not have the correct rights to use this class.');
-        }
-
-        $config = $this->getConfig();
-
-        list($analytics, $token) = $this->getService($config);
-
-        $profile_id = $this->getFirstprofileId($analytics, $config['ga_profile_id']);
-
-        $data = [
-            "locale" => substr($app['locale'], 0, 2),
-            "token" => $token,
-            "profile" => $profile_id,
-            'webpath' => $app['extensions']->get('Bolt/GoogleAnalytics')->getWebDirectory()->getPath(),
-        ];
-
-        $html = $this->renderTemplate("base.twig", $data);
-
-        return new Response($html);
-    }
-
-    protected function getDefaultConfig()
-    {
-        return [
-            'webproperty' => "property-not-set",
-            'backend' => true
-        ];
-    }
-
-    public function insertAnalytics()
-    {
-        $config = $this->getConfig();
-        $app = $this->getContainer();
-
-        $data = [
-            'webproperty' => $config['webproperty']
-        ];
-
-        if ($config['universal']) {
-            $data['domainname'] = $config['universal_domainname'];
-            return $this->renderTemplate("universal.twig", $data);
-        }
-
-        $data['domainname'] = $app['request_stack']->getCurrentRequest()->server->get('HTTP_HOST');
-
-        return $this->renderTemplate("normal.twig", $data);
-    }
-
-    private function getFirstprofileId(&$analytics, $specified_profile_id) {
-        // Get the user's first view (profile) ID.
-
-        // Get the list of profiles for the authorized user.
-        $profiles = $analytics->management_profiles->listManagementProfiles("~all", "~all");
-
-        if (count($profiles->getItems()) > 0) {
-            $items = $profiles->getItems();
-
-            if (! empty($specified_profile_id)) {
-                foreach ($items as $item) {
-                    if ($specified_profile_id == $item->getId()) {
-                        return $specified_profile_id;
-                    }
-                }
-
-                return 'The profile you specified is incorrect. Please re-check your profile ID OR specify no ID';
-            }
-
-            return $items[0]->getId();
-
-        } else {
-            return 'No profiles found for this user. Please check p12 key and email is correct';
-        }
-    }
-
-    private function getService(array $config)
-    {
-        $app = $this->getContainer();
-
-        if (empty($config['service_account_email'])) {
-            return "service_account_email not set in config.yml.";
-        }
-
-        if (empty($config['key_file'])) {
-            return "key_file not set in config.yml.";
-        }
-
-        $service_account_email = $config['service_account_email']; //Email Address
-        $key_file = $config['key_file']; //key.p12
-
-        try {
-            $path = $app['resources']->getPath('extensionsconfig/' . $key_file);
-        } catch (EmptyPathAtomException $e) {
-            return "Please use the filename only, and include the file in app/config/extenstions/.";
-        }
-
-        if (!file_exists($path)) {
-            return "Key file not found in app/config/extenstions/, please place the file there.";
-        }
-
-        require_once(__DIR__.'/../Google/autoload.php');
-
-        // Create and configure a new client object.
-        $client = new \Google_Client();
-        $client->setApplicationName("HelloAnalytics");
-        $analytics = new \Google_Service_Analytics($client);
-
-        // Read the generated client_secrets.p12 key.
-        $key = file_get_contents($path);
-        $cred = new \Google_Auth_AssertionCredentials(
-            $service_account_email,
-            array(\Google_Service_Analytics::ANALYTICS_READONLY),
-            $key
-        );
-
-        $client->setAssertionCredentials($cred);
-
-        if ($client->getAuth()->isAccessTokenExpired()) {
-            $client->getAuth()->refreshTokenWithAssertion($cred);
-        }
-
-        return [$analytics, $client->getAccessToken()];
+        return ['templates'];
     }
 }
---
+++ src/Handler/GoogleAnalyticsHandler.php
@@ -0,0 +1,175 @@
+<?php
+
+
+namespace Bolt\Extension\Bolt\GoogleAnalytics\Handler;
+
+
+use Bolt\Configuration\ResourceManager;
+use Bolt\Extension\Bolt\GoogleAnalytics\Config\Config;
+use Eloquent\Pathogen\Exception\EmptyPathAtomException;
+use Google_Client;
+use Google_Service_Analytics;
+use Google_Auth_AssertionCredentials;
+use Exception;
+
+/**
+ * Class GoogleAnalyticsHandler
+ * @package Bolt\Extension\Bolt\GoogleAnalytics\Handler
+ * Handler to connect to Google Analytics and generate the dashboard
+ */
+class GoogleAnalyticsHandler
+{
+    /** @var Config $config */
+    protected $config;
+
+    /** @var ResourceManager $resource */
+    protected $resource;
+
+    /** @var Google_Client $client */
+    private $client;
+
+    /** @var Google_Service_Analytics $analytics */
+    private $analytics;
+
+    public function __construct(Config $config, ResourceManager $resource)
+    {
+        $this->config = $config;
+        $this->resource = $resource;
+    }
+
+    /**
+     * @return Google_Client
+     * @throws Exception
+     * This function connects to Google Analytics and returns the client
+     */
+    public function connect()
+    {
+        $service_account_email = $this->config->getServiceAccountEmail(); //Email Address
+        $key_file = $this->config->getKeyFile(); //key.p12
+
+        //Verify service account email is in config.yml
+        if (empty($service_account_email)) {
+            throw new Exception("service_account_email not set in config.yml.");
+        }
+
+        //Verify key file is in config.yml
+        if (empty($key_file)) {
+            throw new Exception("key_file not set in config.yml.");
+        }
+
+        //$path may throw error if user specifies FULL path, so catch error and give user a better error message
+        try {
+            $path = $this->resource->getPath('extensionsconfig/' . $key_file);
+        } catch (EmptyPathAtomException $e) {
+            throw new Exception("Please use the filename only, and include the file in app/config/extenstions/.");
+        }
+
+        //Verify key file exists
+        if (!file_exists($path)) {
+            throw new Exception("Key file not found in app/config/extenstions/, please place the file there.");
+        }
+
+        require_once(__DIR__.'/../../Google/autoload.php');
+
+        // Create and configure a new client object.
+        $client = new Google_Client();
+        $client->setApplicationName("Statistics");
+        $analytics = new Google_Service_Analytics($client);
+
+        // Read the generated client_secrets.p12 key.
+        $key = file_get_contents($path);
+        $cred = new Google_Auth_AssertionCredentials(
+            $service_account_email,
+            array(Google_Service_Analytics::ANALYTICS_READONLY),
+            $key
+        );
+
+        $client->setAssertionCredentials($cred);
+
+        if ($client->getAuth()->isAccessTokenExpired()) {
+            $client->getAuth()->refreshTokenWithAssertion($cred);
+        }
+
+        $this->setClient($client);
+
+        $this->setAnalytics($analytics);
+
+        return $client;
+    }
+
+    /**
+     * @return mixed
+     * @throws Exception
+     * This checks to see if the specified profile id is correct,
+     * and if it isn't then throw exception. If nothing set, then it grabs first id.
+     */
+    public function getProfileID() {
+
+        // Get the list of profiles for the authorized user.
+        $profiles = $this->getAnalytics()->management_profiles->listManagementProfiles("~all", "~all");
+
+        //Verify user has profiles
+        if (count($profiles->getItems()) > 0) {
+
+            //Get all of the profiles associated with user
+            $items = $profiles->getItems();
+
+            //Check to see if Bolt Extension configuration has profile id already
+            if (! empty($specified_profile_id)) {
+                //Loop through each profile and check to see if the id is correctly set
+                foreach ($items as $item) {
+                    if ($specified_profile_id == $item->getId()) {
+                        return $specified_profile_id;
+                    }
+                }
+
+                //Throw error is profile ID is incorrect
+                throw new Exception('The profile you specified is incorrect. Please re-check your profile ID OR specify no ID');
+            }
+
+            //Return first profile ID
+            return $items[0]->getId();
+
+        } else {
+            //If no profiles set at all then throw error
+            throw new Exception('No profiles found for this user. Please check p12 key and email is correct');
+        }
+    }
+
+    /**
+     * @return Google_Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param Google_Client $client
+     * @return GoogleAnalyticsHandler
+     */
+    public function setClient($client)
+    {
+        $this->client = $client;
+        return $this;
+    }
+
+    /**
+     * @return Google_Service_Analytics
+     */
+    public function getAnalytics()
+    {
+        return $this->analytics;
+    }
+
+    /**
+     * @param Google_Service_Analytics $analytics
+     * @return GoogleAnalyticsHandler
+     */
+    public function setAnalytics($analytics)
+    {
+        $this->analytics = $analytics;
+        return $this;
+    }
+
+}
